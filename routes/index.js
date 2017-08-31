3d9c99d4253cca5df5566484cc37a3b75875ced3
Roman <roman@Romans-MacBook-Pro.local>
2017-08-31 08:45:28 -0400

    POST and Get requests working

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const models= require("../models/index");
 
 
+
 /////////////////////////////////////////////
 router.get("/", function (req,res){
   models.Chore.findAll({
@@ -24,9 +25,23 @@ router.get("/:id", function (req,res) {
 
   .then(function(data, created){                                     //.then was in place of .spread
     console.log(data);
-    res.render("profile", {user : data});
+    res.render("index", {user : data});
   });
 });
+/////////////////////////////////////////////////////////////Delete The chore from the List
+router.get("/destroy/:id", function (req,res) {
+  models.Chore.destroy({                     ////important method it will find or create, and will save also;
+    where: {
+      id: req.params.id
+    }
+  })
+
+  .then(function(data){                                     //.then was in place of .spread
+    res.redirect("/");
+  });
+
+});
+
 ////////////////////////////////////////////////////This is the edit functionality:
 router.post("/edit/:id", function (req,res) {
   models.Chore.update({
@@ -43,11 +58,45 @@ router.post("/edit/:id", function (req,res) {
 
 });
 
+//////////////////////////////////////////////////////////////
+router.post("/", function (req,res){
+    models.Chore.create({                                          //////creating and saving all in one place
+    name:req.body.name,
+    description: req.body.description,
+    completion: req.body.completion
+  })
+        .then(function(data) {
+          console.log(data);
+          res.redirect("/");
+        });
+      });
+///////////////////////////////////////////////
 
+router.post("/completed/:id", function(req, res) {
+ models.Chore.update({
+   completed:true
+ },
+ {
+   where:{
+     id:req.params.id
+   }
+ }).then(function (data) {
+   res.redirect("/");
+ })
+ });
+//////////////////////////////////////////////// Post to create :
 
-
-
-
+ router.post("/create", function(req, res) {
+  models.Chore.create({
+    Chore: req.body.Chore,
+    completed:false
+  })
+  .then(function(data){
+    data = data;
+    res.redirect("/");
+  })
+});
+///////////////////////////////////////////
 
 
 
